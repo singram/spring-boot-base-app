0f8226ef2ecdf54ae48f6a5a222c9dc9d7916218
Stuart Ingram <ingrams2@upmc.com>
2015-12-02 16:18:28 -0500

    Refactoring of package namespace

--- src/main/java/srai/controller/GreetingController.java
+++ src/main/java/srai/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package hello;
+package srai.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import srai.model.repository.PersonRepository;
 
 @Controller
 public class GreetingController {
--- src/main/java/srai/model/repository/PersonRepository.java
+++ src/main/java/srai/model/repository/PersonRepository.java
@@ -1,10 +1,12 @@
-package hello;
+package srai.model.repository;
 
 import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import srai.model.Person;
 
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
