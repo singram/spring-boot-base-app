f8f5ad127e7b3ef70e76ada2a20795f06bc4e487
Stuart Ingram <ingrams2@upmc.com>
2015-12-07 13:53:33 -0500

    Initial cucumber integration.  gradle build and cucumber now work.  Need to start Spring context and write a real test

---
+++ src/cucumber/java/srai/AbstractDefs.java
@@ -0,0 +1,16 @@
+package srai;
+
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.IntegrationTest;
+//import org.springframework.boot.test.SpringApplicationContextLoader;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = Application.class, loader = SpringApplicationContextLoader.class)
+//@WebAppConfiguration
+//@IntegrationTest
+public abstract class AbstractDefs {
+
+}
