dd2bbc96ad469675e478c21fc6c0f848b63128ee
Stuart Ingram <ingrams2@upmc.com>
2015-12-10 19:31:33 -0500

    Inching closer.

--- src/test/java/srai/integration/PersonAPITests.java
+++ src/test/java/srai/integration/PersonAPITests.java
@@ -3,6 +3,8 @@ package srai.integration;
 import static com.jayway.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.hamcrest.Matchers.equalTo;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.flywaydb.test.annotation.FlywayTest;
 import org.flywaydb.test.junit.FlywayTestExecutionListener;
 import org.junit.Before;
@@ -17,6 +19,7 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
 import com.jayway.restassured.module.mockmvc.response.ValidatableMockMvcResponse;
 
@@ -40,35 +43,50 @@ public class PersonAPITests {
 	}
 
 	@Test
-	public void readPersonRecordFixture() {
+	public void readPersonRecordFixtureSuccess() {
 		given().
 		when().
 		get("/people/{person_id}", 1).
 		then().
-		statusCode(200).
+		contentType(ContentType.JSON).
+		statusCode(HttpServletResponse.SC_OK).
 		body("firstName", equalTo("Stuart"));
 	}
 
 	@Test
-	public void createPersonRecord() {
+	public void createPersonRecordValidationFailure() {
+		given().
+		contentType(ContentType.JSON).
+		body("{ \"firstName\" : \"Frodo\",  \"lastName\" : \"\" }").
+		when().
+		post("/people").
+		then().
+		contentType(ContentType.JSON).
+		statusCode(HttpServletResponse.SC_BAD_REQUEST);
+	}
+
+	@Test
+	public void createPersonRecordSuccess() {
 		//MockMvcResponse response =
-		String personId =
-				given().
-				contentType("application/json").
-				body("{ \"firstName\" : \"Frodo\",  \"lastName\" : \"Baggins\" }").
-				when().
-				post("/people").
-				then().
-				statusCode(201).
-				extract().
-				path("id");
+
+		//		String personId =
+		given().
+		contentType(ContentType.JSON).
+		body("{ \"firstName\" : \"Frodo\",  \"lastName\" : \"Baggins\" }").
+		when().
+		post("/people").
+		then().
+		//		contentType(ContentType.JSON).
+		statusCode(HttpServletResponse.SC_CREATED);
+		//				extract().
+		//				path("id");
 		//				response();
 		//		String r = response.prettyPrint();
 		//		String b = response.body().asString();
 		//		String personId = response.path("id");
-		givenExistingPerson(personId).
-		and().
-		body("firstName", equalTo("Stuart"));
+		//		givenExistingPerson(personId).
+		//		and().
+		//		body("firstName", equalTo("Stuart"));
 	}
 
 	private ValidatableMockMvcResponse givenExistingPerson(String person_id) {
